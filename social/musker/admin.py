eac57a7532cea02dcb398ad78152432c09332ec0
13virat <viratgupta1314@gmail.com>
2023-06-19 22:27:22 +0530

    nc

--- social/musker/admin.py
+++ social/musker/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+from .models import Profile
+
+# Unregister groups
+admin.site.unregister(Group)
+
+
+# mix profile info user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+# Extend User Models
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Display username field on admin page
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+
+# Unregister initial user
+admin.site.unregister(User)
+
+# Reregister user and Profile
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
