ca821f32eeb82fa7204114ced8c39e5ffa5f7b0b
13virat <viratgupta1314@gmail.com>
2023-06-19 23:45:28 +0530

    nn

--- social/musker/urls.py
+++ social/musker/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from musker.models import Profile
+
+
 from . import views
 
-urlpatterns = [path("", views.home, name="home")]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path('profile_list/',views.profile_list,name='profile_list'),
+]
--- social/musker/views.py
+++ social/musker/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from musker.models import Profile
 
 
 def home(request):
     return render(request, 'home.html', {})
+def profile_list(request):
+    return render(request, 'profile_list.html', {})
