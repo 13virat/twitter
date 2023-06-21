c0b97ff7d52269396448a55706b563181acefc43
13virat <viratgupta1314@gmail.com>
2023-06-21 20:21:14 +0530

    vvs

--- social/musker/views.py
+++ social/musker/views.py
@@ -23,6 +23,7 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        meeps=Meep.objects.filter(user_id=pk).order_by("-created_at")
         # Post form logic
         if request.method == "POST":
             # Get current User id
@@ -37,7 +38,7 @@ def profile(request, pk):
             # save profile
             current_user_profile.save()
 
-        return render(request, "profile.html", {"profile": profile})
+        return render(request, "profile.html", {"profile": profile,"meeps":meeps})
     else:
         messages.success(request, ("You must be logged in to view this page"))
         return redirect("home")
